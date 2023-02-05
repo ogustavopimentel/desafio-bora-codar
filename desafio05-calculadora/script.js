df38883fa039d73d75a00bba2ccaf2bf13b5c13f
Gustavo Pimentel <gustavoafpimentel@gmail.com>
2023-02-05 20:15:58 -0300

    Função cancelEntry adicionada

--- desafio05-calculadora/script.js
+++ desafio05-calculadora/script.js
@@ -6,3 +6,8 @@ function insert (num) {
 function clean () {
     document.getElementById('calc').innerHTML = "";
 }
+
+function cancelEntry () {
+    let result = document.getElementById('calc').innerHTML;
+    document.getElementById('calc').innerHTML = result.substring(0, result.length -1);
+}
