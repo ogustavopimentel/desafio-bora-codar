0613018bc1dc67ffdd95fd8269983f525893f5fc
Gustavo Pimentel <gustavoafpimentel@gmail.com>
2023-02-06 22:02:52 -0300

    Função calcute adicionada e atualização das funções insert, clean e cancelEntry

--- desafio05-calculadora/script.js
+++ desafio05-calculadora/script.js
@@ -1,13 +1,51 @@
 
 function insert (num) {
-    document.getElementById('calc').innerHTML += num;
+    let defaultValue = document.getElementById('calc').innerHTML;
+    if (defaultValue == "0") {
+        document.getElementById('calc').innerHTML = "";
+        document.getElementById('calc').innerHTML = num;
+    } else {
+        if (defaultValue.length >= 14) {
+            document.getElementById('calc').innerHTML += "";
+        } else {
+            document.getElementById('calc').innerHTML += num;
+        }
+    }
 }
 
 function clean () {
-    document.getElementById('calc').innerHTML = "";
+    document.getElementById('calc').innerHTML = "0";
+    document.getElementById('result-calc').innerHTML = "0";
 }
 
 function cancelEntry () {
     let result = document.getElementById('calc').innerHTML;
-    document.getElementById('calc').innerHTML = result.substring(0, result.length -1);
+    if (result != "0") {
+        if (result.length == 1) {
+            document.getElementById('calc').innerHTML = "0";
+            document.getElementById('result-calc').innerHTML = "0";
+        } else {
+            document.getElementById('calc').innerHTML = result.substring(0, result.length -1);
+        }  
+    }
+}
+
+function calculate () {
+    try {
+        let result = document.getElementById('calc').innerHTML;
+    let resultAlt = result.replaceAll("÷", "/").replaceAll("x", "*").replaceAll(",", ".");
+    
+    if (result) {
+        let resultCalc = eval(resultAlt);
+        if (resultCalc.toString().length >= 12) {
+            document.getElementById('result-calc').innerHTML = "Valor gigante";
+        } else {
+            document.getElementById('result-calc').innerHTML = resultCalc.toString().replaceAll(".", ",");
+        }
+    }
+    } catch (e) {
+        console.log(e);
+        document.getElementById('calc').innerHTML = "0";
+        document.getElementById('result-calc').innerHTML = "0";
+    }
 }
