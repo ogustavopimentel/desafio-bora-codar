364abd3015b9ffa6c1bd0504369c9e73985017aa
Gustavo Pimentel <gustavoafpimentel@gmail.com>
2023-03-13 21:46:47 -0300

    Estrutura do Conversor de Moedas iniciada

---
+++ desafio09/script.js
@@ -0,0 +1,8 @@
+
+
+var dropdown = document.querySelector('#dropdown');
+
+title.addEventListener("mouseover", function () {
+     var menu = document.querySelector('#menu');
+     menu.style.display = "block";
+});
